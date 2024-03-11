2e9a192d2087f539adbed07436ca3a48a9a33c28
Alessio Rugi <alessiorugi95@gmail.com>
2024-03-11 10:06:38 +0100

    first commit scaffolding

---
+++ Front-end/src/router.js
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from "vue-router";
+const router = createRouter({
+history: createWebHistory(),
+// defizione path pages
+routes: [
+],
+});
+export { router };
