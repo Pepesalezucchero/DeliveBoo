292d3dbbd1928f3f0fc955f87bd3e5ff997f7314
Luca Garizio <garizioluca2@gmail.com>
2024-03-11 10:35:48 +0100

    router & scss set

---
+++ Front-end/src/pages/index.vue
@@ -0,0 +1,7 @@
+<script>
+import { store } from "../store";
+</script>
+<template>
+	<h1>Pagina principale</h1>
+</template>
+<style></style>
--- Front-end/src/router.js
+++ Front-end/src/router.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
+
+import Index from "./pages/index.vue";
+
 const router = createRouter({
-history: createWebHistory(),
-// defizione path pages
-routes: [
-],
+	history: createWebHistory(),
+	// defizione path pages
+	routes: [
+		{
+			path: "/",
+			name: "index",
+			component: Index,
+		},
+	],
 });
 export { router };
