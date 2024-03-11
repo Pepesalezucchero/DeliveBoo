6fce56fb0581ea6c03a369bf7c35788ccd564c97
Domenico-lel <domenicolella2@gmail.com>
2024-03-11 15:52:38 +0100

    upp footer

--- Front-end/src/components/Footer.vue
+++ Front-end/src/components/Footer.vue
@@ -1,5 +1,143 @@
 <script></script>
 <template>
-	<h1>sono il footer</h1>
-</template>
-<style></style>
+	<footer class="bg-dark text-light">
+	  <div class="container">
+		<div class="row my_flex equal-column">
+		  <!-- Prima colonna -->
+		  <div class="col-md-3 col-sm-6 my-color-table mx-2">
+			<h3>Scopri Deliveboo</h3>
+			<ul class="list-unstyled">
+			  <li><a href="#">Invesitori</a></li>
+			  <li><a href="#">Chi siamo</a></li>
+			  <li><a href="#">Ristoranti</a></li>
+			  <li><a href="#">Altro</a></li>
+			  <li><a href="#">Pressroom</a></li>
+			  <li><a href="#">Programmazione</a></li>
+			  <li><a href="#">Design</a></li>
+			  <li><a href="#">Lavora con noi</a></li>
+			  <li><a href="#">Diventa nostro partner</a></li>
+			</ul>
+		  </div>
+  
+		  <!-- Seconda colonna -->
+		  <div class="col-md-3 col-sm-6 my-color-table mx-2">
+			<h3>Note legali</h3>
+			<ul class="list-unstyled">
+			  <li><a href="#">Termini & Condizioni</a></li>
+			  <li><a href="#">Informativa sulla privacy</a></li>
+			  <li><a href="#">Cookies</a></li>
+			  <li><a href="#">Classificazione dei partner</a></li>
+			  <li><a href="#"> Richieste da parte delle Autorita' Publiche</a></li>
+			</ul>
+		  </div>
+  
+		  <!-- Terza colonna -->
+		  <div class="col-md-3 col-sm-6 my-color-table mx-2">
+			<h3>Aiuto</h3>
+			<ul class="list-unstyled">
+			  <li><a href="#">Contatti</a></li>
+			  <li><a href="#">FAQ</a></li>
+			  <li><a href="#">Tipi di cucina</a></li>
+			</ul>
+		  </div>
+  
+		  <!-- Quarta colonna -->
+		  <div class="col-md-3 col-sm-6 my-color-table mx-2">
+			<h5>Porta Deliveboo con te</h5>
+			<ul class="list-unstyled">
+			  <img src="C:\Users\Domenico\Documents\esercizi_boom\DeliveBoo\Front-end\public\img\Store-Download.png" alt="app-store">
+			</ul>
+		  </div>
+		  <div class="bottom_footer">
+			<div>
+				<i class="fa-brands fa-facebook"></i>
+				<i class="fa-brands fa-twitter"></i>
+				<i class="fa-brands fa-instagram"></i>
+			</div>
+			<p>© 2024 Deliveboo</p>
+		  </div>
+		</div>
+	  </div>
+	</footer>
+  </template>
+  
+  <style scoped lang="scss">
+
+  footer {
+	background-color: #2E3333;
+	position: absolute;
+	bottom: 0;
+	width: 100%;
+	height: auto;
+  }
+
+  li {
+		margin-top: 5px;
+	}
+  
+  a {
+	color: white;
+	margin-top: 3px;
+	text-decoration: none;
+	&:hover {
+	  color: #00b8a9;
+	}
+  }
+  
+  .my-color-table {
+	background-color: #434848;
+	margin-top: 20px;
+	padding: 20px;
+	min-height: 355px;
+  }
+  
+  ul img {
+	width: 80%;
+	height: 80%;
+	display: block;
+  }
+  
+  .my_flex {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+  }
+  
+  .equal-column {
+	display: flex;
+  }
+  
+  .equal-column > [class^="col-"] {
+	flex: 1;
+  }
+
+  @media (max-width: 767px) {
+	.equal-column > [class^="col-"] {
+	  flex: 0 0 100%;
+	}
+  }
+
+  .bottom_footer{
+	display: flex;
+	justify-content: space-between;
+	margin-top: 8px;
+  }
+
+  .bottom_footer div > i{
+	margin-right: 20px;
+	font-size: 25px;
+  }
+
+  .bottom_footer p{
+	color: #585C4B;
+  }
+
+  .bottom_footer div > i{
+	margin-top: 3px;
+	text-decoration: none;
+	&:hover {
+	  color: #00b8a9;
+	}
+  }
+  </style>
+  
