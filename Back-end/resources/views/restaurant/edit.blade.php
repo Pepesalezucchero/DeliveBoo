ba5216aac088e0a8d1871312b2554924bf2b9882
Alessio Rugi <alessiorugi95@gmail.com>
2024-03-15 12:42:19 +0100

    fix auth restaurant id

--- Back-end/resources/views/restaurant/edit.blade.php
+++ Back-end/resources/views/restaurant/edit.blade.php
@@ -1,63 +1,71 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="text-center my-4">
-        <h1>Modifica Ristorante {{$restaurant -> name}}</h1>
-        <a class="btn btn-primary" href="{{route('restaurant.index')}}">Torna ai Ristoranti</a>
-    </div>
+    @if (Auth::check() && Auth::user()->id === $restaurant->user_id)
 
-    @if ($errors -> any())
-        <div class="alert alert-danger">
-            <ul class="list-inline">
-                @foreach ($errors -> all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+        <div class="text-center my-4">
+            <h1>Modifica Ristorante {{$restaurant -> name}}</h1>
+            <a class="btn btn-primary" href="{{route('restaurant.index')}}">Torna ai Ristoranti</a>
         </div>
-    @endif
 
-    <form action="{{route('restaurant.update', $restaurant -> id)}}" method="POST" class="container text-center" enctype="multipart/form-data">
-
-        @csrf
-        @method('PUT')
-
-        <div class="shadow-sm card w-50 mx-auto mt-4">
-            <div class="card-body">
-                <div class="mb-3">
-                    <label for="name" class="form-label"><strong>Nome ristorante</strong></label>
-                    <input type="text" class="form-control" name="name" placeholder="Inserisci Nome Ristorante" value="{{ $restaurant -> name }}">
-                </div>
-                <div class="mb-3">
-                    <label for="address" class="form-label"><strong>Indirizzo del tuo ristorante</strong></label>
-                    <input type="text" class="form-control" name="address" placeholder="inserisci l'indirizzo del tuo ristorante" value="{{ $restaurant -> address }}">
-                </div>
-                <div class="mb-3">
-                    <label for="vat_number" class="form-label"><strong>Inserisci la partita IVA</strong></label>
-                    <input type="text" class="form-control" name="vat_number" placeholder="Inserisci la tua partita IVA" value="{{ $restaurant -> vat_number }}">
-                </div>
-                <div class="mb-3">
-                    <label for="image" class="form-label"><strong>Modifica l'immagine del tuo ristorante</strong></label>
-                    <input type="file" class="form-control" name="image" placeholder="Scegli un file" value="{{ $restaurant -> image }}" accept="image/*">
-                </div>
-                <h3 class="mt-3 mb-2">Tipologie:</h3>
-                @foreach ($typologies as $typology)
-                    <div>
-                        <input type="checkbox" name="typologies[]" value="{{ $typology -> id }}" value="{{$typology -> name}}"
-                        
-                            @foreach ($restaurant -> typologies as $typologyTag)
-                                @if ($typologyTag -> id == $typology -> id)
-                                    checked
-                                @endif
-                            @endforeach
-
-                        >
-                        <label for="typology{{ $typology -> id}}">{{ $typology -> name }}</label>
-                    </div>  
-                @endforeach
+        @if ($errors -> any())
+            <div class="alert alert-danger">
+                <ul class="list-inline">
+                    @foreach ($errors -> all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
+        @endif
+
+        <form action="{{route('restaurant.update', $restaurant -> id)}}" method="POST" class="container text-center" enctype="multipart/form-data">
+
+            @csrf
+            @method('PUT')
+
+            <div class="shadow-sm card w-50 mx-auto mt-4">
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="name" class="form-label"><strong>Nome ristorante</strong></label>
+                        <input type="text" class="form-control" name="name" placeholder="Inserisci Nome Ristorante" value="{{ $restaurant -> name }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="address" class="form-label"><strong>Indirizzo del tuo ristorante</strong></label>
+                        <input type="text" class="form-control" name="address" placeholder="inserisci l'indirizzo del tuo ristorante" value="{{ $restaurant -> address }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="vat_number" class="form-label"><strong>Inserisci la partita IVA</strong></label>
+                        <input type="text" class="form-control" name="vat_number" placeholder="Inserisci la tua partita IVA" value="{{ $restaurant -> vat_number }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="image" class="form-label"><strong>Modifica l'immagine del tuo ristorante</strong></label>
+                        <input type="file" class="form-control" name="image" placeholder="Scegli un file" value="{{ $restaurant -> image }}" accept="image/*">
+                    </div>
+                    <h3 class="mt-3 mb-2">Tipologie:</h3>
+                    @foreach ($typologies as $typology)
+                        <div>
+                            <input type="checkbox" name="typologies[]" value="{{ $typology -> id }}" value="{{$typology -> name}}"
+                            
+                                @foreach ($restaurant -> typologies as $typologyTag)
+                                    @if ($typologyTag -> id == $typology -> id)
+                                        checked
+                                    @endif
+                                @endforeach
+
+                            >
+                            <label for="typology{{ $typology -> id}}">{{ $typology -> name }}</label>
+                        </div>  
+                    @endforeach
+                </div>
+            </div>
+
+            <input class="my-1 btn btn-warning mt-4" type="submit" value="Modifica">
+        </form>
+
+    @else()
+        <div class="container text-center mt-3">
+            <h1 class="my-2">Ops, qualcosa è andato storto...</h1>
+            <a class="btn btn-primary" href="{{route('restaurant.show', Auth::user()->id === $restaurant->user_id)}}">Torna al tuo ristorante</a>
         </div>
-
-        <input class="my-1 btn btn-warning mt-4" type="submit" value="Modifica">
-    </form>
-
+    @endif 
 @endsection
