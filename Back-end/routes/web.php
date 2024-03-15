7cb532eb51b88f8904d02f92f799c4255a09672d
saraaffro <saraaffro@gmail.com>
2024-03-15 13:22:13 +0100

    fix: register form

--- Back-end/routes/web.php
+++ Back-end/routes/web.php
@@ -35,42 +35,44 @@ Route::middleware('auth') -> group(function () {
     -> name('restaurant.index');
 
     Route::delete('/restaurant/{id}', [RestaurantController :: class, 'destroy'])
-    -> name('restaurant.delete');
+        -> name('restaurant.delete');
 
     Route::get('/restaurant/{id}/edit', [RestaurantController :: class, 'edit'])
-    -> name('restaurant.edit');
+        -> name('restaurant.edit');
 
     Route::put('/restaurant/{id}/edit', [RestaurantController :: class, 'update'])
-    -> name('restaurant.update');
+        -> name('restaurant.update');
 
     Route::get('/restaurant/create', [RestaurantController :: class, 'create'])
-    -> name('restaurant.create');
+        -> name('restaurant.create');
 
     Route::post('/restaurant/create', [RestaurantController :: class, 'store'])
-    -> name('restaurant.store');
+        -> name('restaurant.store');
 
     Route::get('/restaurant/{id}', [RestaurantController :: class, 'show'])
-    -> name('restaurant.show');
+        -> name('restaurant.show');
 
-    //PIATTI
+
+    // PIATTI
     Route::delete('/dish/{id}', [DishController :: class, 'destroy'])
-    -> name('dish.delete');
+        -> name('dish.delete');
 
     Route::get('/dish/{id}/edit', [DishController :: class, 'edit'])
-    -> name('dish.edit');
+        -> name('dish.edit');
 
     Route::put('/dish/{id}/edit', [DishController :: class, 'update'])
-    -> name('dish.update');
+        -> name('dish.update');
 
     Route::get('/dish/create', [DishController :: class, 'create'])
-    -> name('dish.create');
+        -> name('dish.create');
 
     Route::post('/dish/create', [DishController :: class, 'store'])
-    -> name('dish.store');
+        -> name('dish.store');
 
     Route::get('/dish/{id}', [DishController :: class, 'show'])
-    -> name('dish.show');
+        -> name('dish.show');
 });
 
 require __DIR__.'/auth.php';
+
 
