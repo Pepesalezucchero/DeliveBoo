e3254ec7405b3e97f7cf504e45279b864ef5f0e9
Luca Garizio <garizioluca2@gmail.com>
2024-03-18 14:28:15 +0100

    restaurant filter function added

--- Front-end/src/pages/HomePage.vue
+++ Front-end/src/pages/HomePage.vue
@@ -1,47 +1,61 @@
 <script>
-import { store } from "../store";
 import axios from "axios";
+
 export default {
 	data() {
 		return {
-			store,
 			restaurants: [],
 			typologies: [],
+			selectedTypologies: [],
 		};
 	},
 	methods: {
-		// getTypologies() {
-		// 	axios
-		// 		.get("http://localhost:8000/api/deliveboo/typologies")
-		// 		.then((res) => {
-		// 			console.log(res.data);
-		// 			this.typologies = res.data.typologies;
-		// 		})
-		// 		.catch((err) => {
-		// 			console.log(err);
-		// 		});
-		// },
+		getTypologies() {
+			axios
+				.get("http://localhost:8000/api/deliveboo/typologies")
+				.then((res) => {
+					this.typologies = res.data.typologies;
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
+		getRestaurants() {
+			axios
+				.get("http://localhost:8000/api/deliveboo/restaurants")
+				.then((res) => {
+					this.restaurants = res.data.restaurants;
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
+		filterRestaurants() {
+			// Se non sono selezionate tipologie, mostra tutti i ristoranti
+			if (this.selectedTypologies.length === 0) {
+				this.restaurants = this.getRestaurants();
+			} else {
+				// Filtra i ristoranti in base alle tipologie selezionate
+				this.restaurants = this.restaurants.filter((restaurant) => {
+					// Verifica se ALMENO una delle tipologie selezionate è presente per questo ristorante
+					return this.selectedTypologies.some((selectedTypology) =>
+						// Verifica se ALMENO una delle tipologie del ristorante corrisponde a una tipologia selezionata
+						restaurant.typologies.some(
+							(typology) => typology.id === selectedTypology
+						)
+					);
+				});
+			}
+		},
 	},
 	mounted() {
-		axios
-			.get("http://localhost:8000/api/deliveboo/restaurants")
-			.then((res) => {
-				console.log(res.data);
-				this.restaurants = res.data.restaurants;
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-
-		axios
-			.get("http://localhost:8000/api/deliveboo/typologies")
-			.then((res) => {
-				console.log(res.data);
-				this.typologies = res.data.typologies;
-			})
-			.catch((err) => {
-				console.log(err);
-			});
+		this.getTypologies();
+		this.getRestaurants();
+	},
+	watch: {
+		selectedTypologies() {
+			this.filterRestaurants();
+		},
 	},
 };
 </script>
@@ -53,16 +67,21 @@ export default {
 			<div class="col-12 d-flex justify-content-center flex-wrap">
 				<div
 					class="choices pb-sm-3 pb-lg-0"
-					v-for="(typologies, index) in typologies"
+					v-for="(typology, index) in typologies"
 					:key="index"
 				>
-					<input type="checkbox" class="me-1" name="" value="" />
-					<label class="me-4" for="name">{{ typologies.name }}</label>
+					<input
+						type="checkbox"
+						class="me-1"
+						v-model="selectedTypologies"
+						:value="typology.id"
+					/>
+					<label class="me-4" for="name">{{ typology.name }}</label>
 				</div>
 			</div>
 			<div class="row gy-4">
 				<div
-					class="col-sm-6 col-md-4 col-lg-3 border d-flex justify-content-center"
+					class="col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center transition"
 					v-for="(restaurant, index) in restaurants"
 					:key="index"
 				>
@@ -77,11 +96,11 @@ export default {
 							<p
 								v-for="(typology, index) in restaurant.typologies"
 								:key="index"
-								class="card-text"
+								class="card-text d-flex"
 							>
-								{{ typology.name }}
+								#{{ typology.name }}
 							</p>
-							<a href="#" class="btn btn-primary">vedi piatti</a>
+							<a href="#" class="btn btn-primary">Vedi piatti</a>
 						</div>
 					</div>
 				</div>
