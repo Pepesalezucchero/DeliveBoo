7ebc3f6ea5b14aef3427c9f83fe08c61f16a3ff2
imLukePex <luca.pessione@gmail.com>
2024-03-19 17:12:53 +0100

    fix: landing-page recreate

--- Front-end/src/pages/LandingPage.vue
+++ Front-end/src/pages/LandingPage.vue
@@ -37,7 +37,7 @@
 
     .wrapper {
         position: relative;
-		
+
 			.logo-section {
 				position: absolute;
 				top: 50%;
@@ -51,72 +51,117 @@
 				}
 			}
 
-        .image {
-            width: 100%;
-            height: 100%;
-            background-size: cover;
-            background-position: center;
-            position: absolute;
-            top: 0;
-            left: 0;
-            transition: 0.5s ease all;
-        }
+			.image {
+				width: 100%;
+				height: 100%;
+				background-size: cover;
+				background-position: center;
+				position: absolute;
+				top: 0;
+				left: 0;
+				transition: 0.5s ease all;
+			}
 
-        .delivery {
-            background-image: url('../../public/pizza_wallpaper.jpg');
-        }
+			.delivery {
+				background-image: url('../../public/pizza_wallpaper.jpg');
+			}
 
-        .right {
-            background: url("../../public/restaurant-wallpaper.png") no-repeat scroll center;
-            background-size: cover;
-            width: 50%;
-            height: 100vh;
-            transition: all 0.5s ease-in-out;
+			.business {
+				background-image: url('../../public/restaurant-business-wallpaper.jpg');
+			}
+	}
 
-            .right-wrapper {
-                background-color: rgb(71, 53, 44, 0.4);
-                height: 100%;
+	.wrapper .caption {
+		position: relative;
+		z-index: 10;
+		color: #ffff;
+		text-align: center;
 
-                .right-content {
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                    text-align: center;
-                    height: 100%;
-                    transition: all 0.5s ease-in-out;
-                    cursor: pointer;
-                    padding: 4rem 4rem;
+		h1 {
+			font-size: 50px;
+			text-transform: uppercase;
+			letter-spacing: 0.10em;
+			margin-bottom: 20px;
+		}
 
-                    .major-title {
-                        font-size: 4rem;
-                        color: white;
-                        text-shadow: 0 0 5px black;
-                    }
+		p {
+			color: #d9d9d9;
+			font-size: 18px;
+			margin-bottom: 30px;
+		}
 
-                    .major-description {
-                        font-size: 1.5rem;
-                        color: white;
-                    }
-                }
-            }
-        }
-    }
+		.button {
+			background: #f68f00;
+			color: #000;
+			padding: 14px 20px;
+			text-decoration: none;
+			font-weight: 600;
+			font-size: 14px;
+			text-transform: uppercase;
+			letter-spacing: 0.10em;
+			transition: 0.5s ease all;
+		}
 
-    @media screen and (max-width: 768px) {
-        .cont {
-            display: block;
-            .logo-section {
+		.button:hover {
+			background-color: #000;
+			color: #ffff;
+		}
+	}
 
-                img {
-                    display: none;
-                }
-            }
-            .left, .right {
-                width: 100%;
-                height: 50vh;
-            }
-        }
+	.wrapper .left,
+	.wrapper .right {
+		width: 100%;
+		height: 50vh;
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		overflow: hidden;
+	}
+
+	.wrapper .side::before {
+		background: linear-gradient(0deg, rgba(0,0,0,0.6979166666666667) 0%, rgba(0,0,0,0.5) 100%);
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		z-index: 1;
+		content: '';
+		display: block;
+	}
+
+	.wrapper .left:hover .image,
+	.wrapper .right:hover .image {
+		transform: scale(1.2);
+	}
+
+
+    @media screen and (min-width: 1200px) {
+
+		.wrapper {
+			display: flex;
+
+			.logo-section {
+				position: absolute;
+				top: 17%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+				z-index: 20;
+				
+				img {
+					max-width: 350px;
+					height: auto;
+				}
+			}
+		}
+        
+		.wrapper .left,
+		.wrapper .right {
+
+			width: 50%;
+			height: 100vh;
+		}
     }
 </style>
 
