b06e5f21231d48b30f97ccde97842f2bd7dc64e4
saraaffro <saraaffro@gmail.com>
2024-03-19 17:12:01 +0100

    fix: layout menu page

--- Front-end/src/components/Menu.vue
+++ Front-end/src/components/Menu.vue
@@ -31,7 +31,7 @@ export default {
 
 <template>
 	<div class="container">
-		<div class="row gy-3 mt-5">
+		<!-- <div class="row gy-3 mt-5">
 			<h2 class="text-center">I nostri piatti: {{ dishes.length }}</h2>
 			<div class="col-3" v-for="(dish, index) in dishes" :key="index">
 				<div class="card">
@@ -46,8 +46,79 @@ export default {
 					</div>
 				</div>
 			</div>
+		</div> -->
+		<div class="row mt-4 mb-5 align-items-center">
+			<div class="col-4">
+				<div class="img-container">
+					<img src="https://www.italiaatavola.net/images/contenutiarticoli/kuiri-food-delivery.jpeg" alt="img-ristorante">
+				</div>
+			</div>
+			<div class="col-8">
+				<h2>Nome Ristorante</h2>
+				<p class="type">Tipologie Ristorante</p>
+			</div>
 		</div>
+		<div class="row justify-content-between">
+			<div class="col-9 d-flex flex-wrap justify-content-between">
+				<div class="col-12 col-md-6 d-flex align-items-center justify-content-between mb-4 dish" v-for="(dish, index) in dishes" :key="index">
+					<div class="col-10 d-flex align-items-center">
+						<div class="col-5 me-3">
+							<div class="img-dish">
+								<img src="https://www.italiaatavola.net/images/contenutiarticoli/kuiri-food-delivery.jpeg" alt="img piatto">
+							</div>
+						</div>
+						<div class="col-5">
+							<h4>{{ dish.name }}</h4>
+							<p>{{ dish.price }} &euro;</p>
+						</div>
+						<div class="col-2">
+							<i class="fa-solid fa-plus"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-3 cart">
+				Carrello
+			</div>
+		</div>
+		
 	</div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.img-container{
+	width: 100%;
+
+	img{
+		width: 100%;
+	}
+}
+
+.dish{
+	border: 1px solid black;
+	padding: 10px;
+
+	.img-dish{
+		width: 100%;
+
+		img{
+			width: 100%;
+		}
+	}
+	.fa-solid{
+		border: 1px solid black;
+		border-radius: 50%;
+		padding: 10px;
+		margin: 0 10px;
+		font-size: 22px;
+		cursor: pointer;
+	}
+}
+
+.cart{
+	text-align: center;
+	border: 1px solid black;
+	height: 200px;
+}
+
+</style>
