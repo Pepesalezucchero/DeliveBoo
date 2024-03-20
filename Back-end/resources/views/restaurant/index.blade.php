c8030fb091bd665a6d34bb654920b2daef306af9
saraaffro <saraaffro@gmail.com>
2024-03-20 17:42:32 +0100

    fix: restaurant index layout

--- Back-end/resources/views/restaurant/index.blade.php
+++ Back-end/resources/views/restaurant/index.blade.php
@@ -16,29 +16,29 @@
 
                 <div class="row justify-content-between align-items-center pt-5">
                     <div class="col-md-12 col-lg-7">
-                        @if($restaurant -> image)
-                            <img class="card-img-top shadow" style="height:400px;" src="{{ asset('storage/' . $restaurant -> image) }}" alt="(immagine ristorante {{$restaurant -> name}})">   
+                        @if($restaurant->image)
+                            <img class="card-img-top shadow" style="height:400px;" src="{{ asset('storage/' . $restaurant->image) }}" alt="(immagine ristorante {{$restaurant->name}})">   
                         @else
                             <img class="card-img-top shadow" style="height:400px;" src="{{asset('images/ristodeliveboo.png')}}" alt="immagine ristorante deliveboo">
                         @endif
                     </div>
                     <div class="col-md-12 col-lg-5">
-                        <h2 class="text-center d-inline-block">{{$restaurant -> name}}</h2> <a href="{{ route('restaurant.orders')}}" class="btn btn-primary ms-3 mb-2">Vedi Ordini</a>
-                        <h6 class="card-text"><strong>Indirizzo: </strong>{{$restaurant -> address}}</h6>
-                        <h6 class="card-text my-4"><strong>Partita IVA: </strong>{{$restaurant -> vat_number}}</h6>
+                        <h2 class="text-center d-inline-block">{{$restaurant->name}}</h2> <a href="{{ route('restaurant.orders')}}" class="btn btn-primary ms-3 mb-2">Vedi Ordini</a>
+                        <h6 class="card-text"><strong>Indirizzo: </strong>{{$restaurant->address}}</h6>
+                        <h6 class="card-text my-4"><strong>Partita IVA: </strong>{{$restaurant->vat_number}}</h6>
                         <div class="typology d-flex justify-content-center">
                             <h6 class="card-text text-center me-1"><strong>Tipologie: </strong></h6>
-                                @foreach ($restaurant -> typologies as $typology)
-                            <div>
-                                <h6 class="mx-1">#{{$typology -> name}}</h6>
-                            </div>
+                            @foreach ($restaurant->typologies as $typology)
+                                <div>
+                                    <h6 class="mx-1">#{{$typology->name}}</h6>
+                                </div>
                             @endforeach
                         </div>
                         <div class="mt-3">
                             <a class="btn btn-warning" href="{{ route('restaurant.edit', $restaurant->id) }}">Modifica Ristorante</a>
-                            <a class="btn btn-success"  href="{{route('dish.create')}}">Aggiungi un piatto</a>    
+                            <a class="btn btn-success" href="{{route('dish.create')}}">Aggiungi un piatto</a>    
                             <div class="my-3">
-                                <form id="deleteRestaurantForm" action="{{ route('restaurant.delete', $restaurant -> id) }}" method="POST">
+                                <form id="deleteRestaurantForm" action="{{ route('restaurant.delete', $restaurant->id) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
                                     <input class="btn btn-danger" type="submit" value="Elimina Ristorante">
@@ -49,49 +49,49 @@
                                     <button id="cancelDelete" class="btn btn-secondary">Annulla</button>
                                 </div>
                             </div>
-
-                          
                         </div>       
                     </div>      
                 </div>
 
-
                 <div class="row mt-5 p-0 m-0">
-                    <h2 class="text-center">I tuoi piatti: {{count($restaurant -> dishes)}}</h2>
-                    @foreach ($restaurant -> dishes as $dish)
-                        <div class="col-sm-12 col-lg-4 col-xl-4 col-xxl-3 mt-4 ">
-                            <div class="card mb-sm-5 mb-lg-2">
-                                <div class="card-text text-center">
-                                    @if ($dish -> image)
-                                        <img class="card-img-top" src="{{ asset('storage/' . $dish -> image) }}" alt="(immagine piatto {{$dish -> name}})">
-                                    @else
-                                        <img class="card-img-top" src="{{asset('images/piattodeliveboo.png')}}" alt="immagine piatto deliveboo">
-                                    @endif
-                                   
-                                    <div class="card-text border-1">
-                                        <h5 class="my-3"> {{$dish -> name}}</h5>  
-                                    </div>
-                                    <a class="btn btn-primary mb-4" href="{{route ('dish.show', $dish -> id) }}">Mostra i dettagli</a>  
-                                    <div class="d-flex justify-content-center mb-3">
-                                        <a class="btn btn-warning" href="{{route('dish.edit', $dish -> id)}}">Modifica</a>
-                                        <div class="col-sm-12 col-xl-4 my-sm-3 my-xl-0">
-                                            <form id="deleteDishForm" class="d-inline" action="{{ route('dish.delete', $dish->id) }}" method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <input type="submit" value="Cancella" class="btn btn-danger">
-                                            </form>
-                                            <div class="position" id="deleteConfirmation" style="display: none; margin-top:20px;">
-                                                <p class="mt-5 pt-3">Sei sicuro di voler cancellare questo piatto?</p>
-                                                <button id="confirmDelete" class="btn btn-danger">Conferma</button>
-                                                <button id="cancelDelete" class="btn btn-secondary">Annulla</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
+                    <h2 class="text-center">I tuoi piatti: {{count($restaurant->dishes)}}</h2>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Immagine</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Descrizione</th>
+                                <th scope="col">Prezzo</th>
+                                <th scope="col">Azioni</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($restaurant->dishes as $dish)
+                                <tr>
+                                    <td>
+                                        @if ($dish->image)
+                                            <img src="{{asset($dish -> image)}}" alt="immagine piatto">
+                                        @else
+                                            <img style="width: 100px" src="{{asset('images/piattodeliveboo.png')}}" alt="immagine piatto">
+                                        @endif
+                                        
+                                    </td>
+                                    <td>{{$dish->name}}</td>
+                                    <td>{{$dish->description}}</td>
+                                    <td>{{$dish->price}} €</td>
+                                    <td>
+                                        <a class="btn btn-primary" href="{{route ('dish.show', $dish->id) }}">Mostra dettagli</a>  
+                                        <a class="btn btn-warning" href="{{route('dish.edit', $dish->id)}}">Modifica</a>
+                                        <form id="deleteDishForm" class="d-inline" action="{{ route('dish.delete', $dish->id) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <input type="submit" value="Cancella" class="btn btn-danger">
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>   
         
             @endif
