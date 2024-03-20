9a9b3537c1fd414d886cc48453cee1c2848b8df7
Luca Garizio <garizioluca2@gmail.com>
2024-03-20 16:02:22 +0100

    add img on notfound page

--- Front-end/src/components/TypologyNotFound.vue
+++ Front-end/src/components/TypologyNotFound.vue
@@ -1,13 +1,50 @@
 <template>
-	<div class="row justify-content-center mt-5">
-		<div class="col-8 text-center">
-			<h1>
-				ci dispiace i ristoranti con le tipologie selezionate non sono presenti
-			</h1>
+	<section>
+		<div class="container py-5">
+			<div class="row justify-content-center align-items-center">
+				<div class="col-8 text-center">
+					<h1>
+						ci dispiace i ristoranti con le tipologie selezionate non sono
+						presenti
+					</h1>
+					<div class="img-container py-5">
+						<img
+							src="../../public/img/sfondo-riso-deliveboo_.png"
+							alt="ristorante non trovato"
+						/>
+					</div>
 
-			<router-link class="btn btn-secondary mt-3" to="/homepage">
-				Torna alla home Page</router-link
-			>
+					<router-link class="btn" to="/homepage">
+						Torna alla home Page</router-link
+					>
+				</div>
+			</div>
 		</div>
-	</div>
+	</section>
 </template>
+
+<style lang="scss" scoped>
+section {
+	.row {
+		width: 100%;
+		h1 {
+			color: black;
+		}
+
+		img {
+			width: 100%;
+		}
+
+		.btn {
+			background-color: #e69c23;
+			color: white;
+		}
+	}
+
+	@media all and (min-width: 992px) {
+		img {
+			max-width: 500px;
+		}
+	}
+}
+</style>
